54d722dbb09ca88ef3d143241337876f3c54f4d7
A S Vijayavitthala <96972960+vv-01@users.noreply.github.com>
2023-06-20 22:56:48 +0530

    Updated to real time sending and receiving messages !

--- app/src/main/java/com/asv/holachat/ChatDetailActivity.java
+++ app/src/main/java/com/asv/holachat/ChatDetailActivity.java
@@ -1,15 +1,26 @@
 package com.asv.holachat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 
+import com.asv.holachat.Adapter.chatAdapter;
+import com.asv.holachat.Models.messageModel;
 import com.asv.holachat.databinding.ActivityChatDetailBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class ChatDetailActivity extends AppCompatActivity {
 
@@ -43,5 +54,71 @@ public class ChatDetailActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        final ArrayList<messageModel> messageModels = new ArrayList<>();
+        final chatAdapter chatAdapter = new chatAdapter(messageModels,this,recieveId);
+        binding.chatRecyclerView.setAdapter(chatAdapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.chatRecyclerView.setLayoutManager(layoutManager);
+
+        final String senderRoom = senderId + recieveId;
+        final String receiverRoom = recieveId + senderId;
+
+
+        database.getReference().child("chats")
+                .child(senderRoom)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        messageModels.clear();
+                        for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                            messageModel model = snapshot1.getValue(messageModel.class);
+                            model.setMessageId(snapshot1.getKey());
+                            messageModels.add(model);
+                        }
+                        chatAdapter.notifyDataSetChanged();
+
+                        chatAdapter.notifyDataSetChanged();
+                    }
+
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        binding.send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = binding.enterMessage.getText().toString();
+                final messageModel model = new messageModel(senderId,message);
+                model.setTimestamp(new Date().getTime());
+                binding.enterMessage.setText("");
+
+                database.getReference().child("chats")
+                        .child(senderRoom)
+                        .push()
+                        .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+
+                                    database.getReference().child("chats")
+                                            .child(receiverRoom)
+                                            .push()
+                                            .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void unused) {
+
+                                        }
+                                    });
+                            }
+                        });
+
+            }
+        });
+
     }
 }
